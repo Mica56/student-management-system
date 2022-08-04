805da5ff70658757b77f1332f2a04cfdaf7f942b
Mica56 <tayotomichael18@gmail.com>
2022-08-04 20:24:54 +0800

    updated db

--- student_enrollment/student.sql
+++ student_enrollment/student.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2020 at 07:22 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.2
+-- Generation Time: Aug 04, 2022 at 02:23 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -44,11 +43,10 @@ CREATE TABLE `student_info` (
 --
 
 INSERT INTO `student_info` (`id`, `name`, `roll`, `class`, `city`, `pcontact`, `photo`, `datetime`) VALUES
-(41, 'Ajharul Islam', 444433, 'BSCS3-1', 'House#15, Ward#', '01944444444', '4444332020-06-06-06-58.jpg', '2020-06-06 16:17:58'),
-(43, 'Majhar Rakib', 444439, 'BSCS3-2', 'House#1eww', '01812888858', '4444392020-06-06-06-53.jpg', '2020-06-06 16:18:53'),
-(44, 'kutub ussin', 443322, 'BSCS3-4', 'Dhaka, Banglade', '01797159600', '4433222020-06-06-06-28.jpg', '2020-06-06 16:19:28'),
-(45, 'Shirin Akter', 443342, 'BSCS3-2', 'Dhaka, Banglade', '01797159600', '4433422020-06-06-06-51.jpg', '2020-06-06 16:19:51'),
-(47, 'utfol kumar das', 443353, 'BSCS3-2', 'Dhaka, Banglade', '01814270541', '4433532020-06-06-06-32.jpg', '2020-06-06 16:21:32');
+(41, 'Ajharul Islam', '444433', 'BSCS3-1', 'House#15, Ward#', '01944444444', '4444332020-06-06-06-58.jpg', '2020-06-06 16:17:58'),
+(45, 'Shirin Akter', '443342', 'BSCS3-2', 'Dhaka, Banglade', '01797159600', '4433422020-06-06-06-51.jpg', '2020-06-06 16:19:51'),
+(47, 'utfol kumar das', '443353', 'BSCS3-2', 'Dhaka, Banglade', '01814270541', '4433532020-06-06-06-32.jpg', '2020-06-06 16:21:32'),
+(48, 'Micaela Cerilla', '2019-06342-MN-0', 'BSCS3-1N', 'Taguig City', '09123456789', '2019-06342-MN-02022-08-04-08-52.jpg', '2022-08-04 11:17:52');
 
 -- --------------------------------------------------------
 
@@ -73,13 +71,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `photo`, `status`, `datetime`) VALUES
 (10, 'Ajharul Islam', 'dedke@gmail.com', 'sdsdq232323', '1bbd886460827015e5d605ed44252251', 'sdsdq2323ee23.jpg', 'inactive', '2020-06-08 11:49:10'),
-(14, 'Majhar Rakib', 'ded222ke@gmail.com', 'yescablessdwwewe', '618e02fc80fa3a0bd41d65f5b54a11fc50426d12', 'yescablessdwwewe.jpg', 'inactive', '2020-06-08 11:49:10'),
-(15, 'Ajharul Islam', 'sdsdsdsdsdsd@gmail.com', 'mdrashed38', '618e02fc80fa3a0bd41d65f5b54a11fc50426d12', 'mdrashed38.jpg', 'inactive', '2020-06-08 11:49:10'),
-(16, 'Ajharul Islam', 'rajrashed42s1@gmail.com', 'yescablessasasas', '618e02fc80fa3a0bd41d65f5b54a11fc50426d12', 'yescablessasasas.jpg', 'inactive', '2020-06-08 11:49:10'),
-(17, 'Ajharul Islam', 'rajrashed42sdsd1@gmail.com', 'yescables133232323', '618e02fc80fa3a0bd41d65f5b54a11fc50426d12', 'yescables133232323.jpg', 'inactive', '2020-06-08 11:49:10'),
 (18, 'Ajharul Islam', 'djforhad@gmail.com', 'rasheddd', '618e02fc80fa3a0bd41d65f5b54a11fc50426d12', 'rasheddd21-06-20-06-2020steo.jpg', 'active', '2020-06-08 11:49:10'),
 (19, 'kus', 'kus@gmail.com', 'kus12345', '1f82ea75c5cc526729e2d581aeb3aeccfef4407e', 'kus12345.jpg', 'inactive', '2020-06-08 11:49:10'),
-(20, 'Ajharul Islam', 'janatakumar@gmail.com', 'kumarkosl', '618e02fc80fa3a0bd41d65f5b54a11fc50426d12', 'kumarkosl.jpg', 'inactive', '2020-06-08 11:49:10');
+(21, 'admin', 'admin@gmail.com', 'admin1234', '87a37f18c083b69676c18761b7a1df63d4b2f2ae', 'admin1234.png', 'active', '2022-08-04 11:03:34');
 
 --
 -- Indexes for dumped tables
@@ -107,13 +101,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `student_info`
 --
 ALTER TABLE `student_info`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
